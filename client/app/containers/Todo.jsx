be30ccb1ff54f5782e819b964570df68bcd21ac4
Alex Overbeck <alexoverbeck@gmail.com>
2015-11-24 11:08:58 -0500

    Move todo components into sub directory

--- client/app/containers/Todo.jsx
+++ client/app/containers/Todo.jsx
@@ -1,8 +1,8 @@
 import React, { Component, PropTypes } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import Header from '../components/Header'
-import MainSection from '../components/MainSection'
+import Header from '../components/todos/Header'
+import MainSection from '../components/todos/MainSection'
 import * as TodoActions from '../actions/todos'
 
 class App extends Component {
