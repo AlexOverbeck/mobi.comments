e94e9dfdef196c2a0a38ef0fbfce67b9cc15d446
Alex Overbeck <alexoverbeck@gmail.com>
2016-11-08 07:26:50 -0500

    set redis config

--- Gemfile
+++ Gemfile
@@ -20,7 +20,7 @@ gem 'uglifier', '>= 1.3.0'
 # gem 'therubyracer', platforms: :ruby
 
 # Use Redis adapter to run Action Cable in production
-# gem 'redis', '~> 3.0'
+gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.web_socket_server_url = 'wss://mobi-comments.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = ['https://mobi-comments.herokuapp.com', 'http://mobi-comments.herokuapp.com']
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -37,8 +36,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.web_socket_server_url = 'wss://mobi-comments.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ['https://mobi-comments.herokuapp.com', 'http://mobi-comments.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
